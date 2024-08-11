6be1302175400e4399dd558e8177b26a4449ebee
sivateja18 <sivateja1213@gmail.com>
2024-08-12 02:34:24 +0530

    ready to deploy

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+const dbConfig = require('./config/dbConfig')
+const userRoutes = require('./routes/userRoutes.js')
+const movieRoutes = require('./routes/movieRoutes.js')
+const theatreRoutes = require('./routes/theatreRoutes.js')
+const showRoutes = require('./routes/showRoutes.js')
+const bookingRoutes = require('./routes/bookingRoutes.js')
+
+app.use(express.static('build'));
+app.use(express.json())
+app.use('/api/users', userRoutes.router)
+app.use('/api/movies', movieRoutes.router)
+app.use('/api/theatres', theatreRoutes.router)
+app.use('/api/shows', showRoutes.router)
+app.use('/api/bookings/',bookingRoutes.router)
+app.listen(8080, () => {
+    console.log("Server has started...");
+})
